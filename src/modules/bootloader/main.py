d6b3b8d30db5748e3133d397ede8a87664b7e604
Peter Jung <admin@ptr1337.dev>
2024-11-06 14:43:53 +0100

    Revert "Change rEFInd partition layout, switch to new calamares schema and fix refind installation. (#11)"
    This reverts commit 60bce58e4f8850d41e98367f58884be9114c234b.

--- src/modules/bootloader/main.py
+++ src/modules/bootloader/main.py
@@ -833,7 +833,7 @@ def install_refind(efi_directory):
     kernel_params = " ".join(get_kernel_params(uuid))
     conf_path = os.path.join(installation_root_path, "boot/refind_linux.conf")
 
-    libcalamares.utils.host_env_process_output(["refind-install", "--root", installation_root_path])
+    check_target_env_call(["refind-install"])
 
     with open(conf_path, "r") as refind_file:
         filedata = [x.strip() for x in refind_file.readlines()]
