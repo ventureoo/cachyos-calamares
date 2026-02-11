a729f3180ecf43948e904202475c8855fffc45d2
Vasiliy Stelmachenok <ventureo@cachyos.org>
2026-02-11 15:51:17 +0300

    users: Set presets based on platform type
    Signed-off-by: Vasiliy Stelmachenok <ventureo@cachyos.org>

--- src/modules/users/Config.cpp
+++ src/modules/users/Config.cpp
@@ -22,6 +22,7 @@
 #include "utils/Permissions.h"
 #include "utils/String.h"
 #include "utils/StringExpander.h"
+#include "utils/System.h"
 #include "utils/Variant.h"
 
 #include <QCoreApplication>
@@ -1050,8 +1051,20 @@ Config::setConfigurationMap( const QVariantMap& configurationMap )
     updateGSAutoLogin( doAutoLogin(), loginName() );
     checkReady();
 
-    ApplyPresets( *this, configurationMap ) << "fullName"
-                                            << "loginName";
+    bool ok = false;
+    auto platformOverrides = Calamares::getSubMap( configurationMap, "platform", ok );
+
+    if ( ok )
+    {
+        QString targetPlatform = Calamares::System::instance()->getTargetPlatform();
+        auto preset = Calamares::getSubMap( platformOverrides, targetPlatform, ok );
+
+        if ( ok )
+        {
+            ApplyPresets( *this, preset ) << "fullName"
+                                          << "loginName";
+        }
+    }
 }
 
 void
